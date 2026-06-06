879439fd42050373658f8ca1d923234ea16db093
Aravind <aravindcherry16@gmail.com>
2026-06-06 20:09:55 +0530

    added chrome optionbs to baseclass

--- BDDFramework/src/test/java/utility/baseclass.java
+++ BDDFramework/src/test/java/utility/baseclass.java
@@ -1,7 +1,7 @@
 package utility;
 
 import org.openqa.selenium.PageLoadStrategy;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -12,9 +12,17 @@ public class baseclass extends Elements {
 	
 	public void Login() {
 		if(FrameWorkconstants.browser == "chrome") {
-			  ChromeOptions chromeOptions = new ChromeOptions();
-			    chromeOptions.setPageLoadStrategy(PageLoadStrategy.NONE);
-			    mainDriver  = new ChromeDriver(chromeOptions);
+			    //chromeOptions.setPageLoadStrategy(PageLoadStrategy.NONE);
+			    //mainDriver  = new ChromeDriver(chromeOptions);
+				 ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Run in headless mode for CI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        
+        WebDriver driver = new ChromeDriver(options);
+        Elements.mainDriver = driver;  // Initialize the static driver
+        driver.manage().timeouts().implicitlyWait(10, java.util.concurrent.TimeUnit.SECONDS);
 			
 		}
 		mainDriver.get(FrameWorkconstants.url);
